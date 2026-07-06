07512deb6ad0242b8025b31a0e29f5dd3cf68c85
永恒瞬间 <130402124@qq.com>
2026-07-06 10:13:25 +0800

    回退video版本，解决安卓打包问题

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.tasks.compile.JavaCompile
+
 allprojects {
     repositories {
         google()
@@ -15,6 +18,28 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// video_player_android 2.7.1 pins Java 8 and passes -Werror, which fails on newer JDKs.
+subprojects {
+    if (name != "video_player_android") {
+        return@subprojects
+    }
+    pluginManager.withPlugin("com.android.library") {
+        extensions.configure<LibraryExtension>("android") {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+        tasks.withType<JavaCompile>().configureEach {
+            options.compilerArgs.removeAll(listOf("-Werror"))
+            if (!options.compilerArgs.contains("-Xlint:-options")) {
+                options.compilerArgs.add("-Xlint:-options")
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
